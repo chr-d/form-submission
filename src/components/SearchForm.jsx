4ec6c712e8e5e3f5b7a595e0dbe76465f5b3568e
chr-d <275697199+chr-d@users.noreply.github.com>
2026-08-16 15:33:11 +0200

    add solution for Search

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,41 +1,40 @@
-const SearchForm = () => {
+import { useState } from "react";
+
+const SearchForm = ({ formAction, isPending }) => {
+  const [formState, setFormState] = useState({
+    category: "",
+    minPrice: "",
+    maxPrice: "",
+    query: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormState((prev) => ({ ...prev, [name]: value }));
+  };
   return (
-    <form className='w-full max-w-4xl'>
-      <fieldset className='bg-base-200 border border-base-300 rounded-box p-4'>
-        <legend className='fieldset-legend mb-2'>Search Products</legend>
-        <div className='grid grid-cols-1 md:grid-cols-5 gap-4 items-end'>
-          <div className='flex flex-col'>
-            <label className='label text-sm font-medium'>Category</label>
-            <input className='input input-bordered w-full' name='category' placeholder='Category' />
+    <form action={formAction} className="w-full max-w-4xl">
+      <fieldset className="bg-base-200 border border-base-300 rounded-box p-4">
+        <legend className="fieldset-legend mb-2">Search Products</legend>
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 items-end">
+          <div className="flex flex-col">
+            <label className="label text-sm font-medium">Category</label>
+            <input className="input input-bordered w-full" name="category" placeholder="Category" onChange={handleChange} value={formState.category} />
           </div>
-          <div className='flex flex-col'>
-            <label className='label text-sm font-medium'>Min Price</label>
-            <input
-              type='number'
-              className='input input-bordered w-full'
-              name='minPrice'
-              placeholder='Min'
-            />
+          <div className="flex flex-col">
+            <label className="label text-sm font-medium">Min Price</label>
+            <input type="number" className="input input-bordered w-full" name="minPrice" placeholder="Min" onChange={handleChange} value={formState.minPrice} />
           </div>
-          <div className='flex flex-col'>
-            <label className='label text-sm font-medium'>Max Price</label>
-            <input
-              type='number'
-              className='input input-bordered w-full'
-              name='maxPrice'
-              placeholder='Max'
-            />
+          <div className="flex flex-col">
+            <label className="label text-sm font-medium">Max Price</label>
+            <input type="number" className="input input-bordered w-full" name="maxPrice" placeholder="Max" onChange={handleChange} value={formState.maxPrice} />
           </div>
-          <div className='flex flex-col'>
-            <label className='label text-sm font-medium'>Description</label>
-            <input
-              className='input input-bordered w-full'
-              name='query'
-              placeholder='Search keyword'
-            />
+          <div className="flex flex-col">
+            <label className="label text-sm font-medium">Description</label>
+            <input className="input input-bordered w-full" name="query" placeholder="Search keyword" onChange={handleChange} value={formState.query} />
           </div>
-          <div className='flex justify-end md:items-end'>
-            <button type='submit' className='btn btn-neutral w-full md:w-auto mt-1 md:mt-0'>
+          <div className="flex justify-end md:items-end">
+            <button disabled={isPending} type="submit" className="btn btn-neutral w-full md:w-auto mt-1 md:mt-0">
               Search
             </button>
           </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,29 @@
-import { Instructions, SearchForm, SearchResults } from '../components';
-import { searchProducts } from '../api/index.js';
+import { use, useActionState } from "react";
+import { searchProducts } from "../api/index.js";
+import { Instructions, SearchForm, SearchResults } from "../components";
 
 const productsPromise = searchProducts();
 
 const Search = () => {
+  const submitAction = async (prevState, formData) => {
+    // have to pass numbers as undefined if empty or they will be 0
+    // category is case-sensitive
+    // query only matches title
+    return await searchProducts({ category: formData.get("category"), minPrice: formData.get("minPrice") !== "" ? Number(formData.get("minPrice")) : undefined, maxPrice: formData.get("maxPrice") !== "" ? Number(formData.get("maxPrice")) : undefined, query: formData.get("query") });
+  };
+
+  const [state, formAction, isPending] = useActionState(submitAction, use(productsPromise));
+
   return (
-    <div className='flex flex-col items-center'>
-      <SearchForm />
-      <SearchResults productsPromise={productsPromise} />
-      <Instructions path='/search.md' />
+    <div className="flex flex-col items-center">
+      <SearchForm formAction={formAction} isPending={isPending} />
+      {state.error && (
+        <p role="alert" className="mt-2 text-red-600 font-medium">
+          {state.error}
+        </p>
+      )}
+      <SearchResults products={state.products} />
+      <Instructions path="/search.md" />
     </div>
   );
 };
